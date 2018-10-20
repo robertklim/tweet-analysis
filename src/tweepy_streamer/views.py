93545cd7ecd535c2d0e47dd46b2893e96923214e
Robert Klim <kontakt.robert.klim@gmail.com>
2018-10-20 14:24:30 +0200

    Connect to Twitter and stream tweets filtered by keywords to Std Out

--- src/tweepy_streamer/views.py
+++ src/tweepy_streamer/views.py
@@ -4,9 +4,34 @@ from django.views.generic import (
     View,
 )
 
+from tweepy import OAuthHandler
+from tweepy import Stream
+from tweepy.streaming import StreamListener
+
+from . import twitter_credentials
+
+class StdOutListener(StreamListener):
+
+    def on_data(self, data):
+        print(data)
+        return True
+
+    def on_error(self, status):
+        print(status)
+
 class StreamerView(View):
     template_name = 'tweepy_streamer/streamer.html'
 
     def get(self, request, *args, **kwargs):
+        
+        listener = StdOutListener()
+        
+        auth = OAuthHandler(twitter_credentials.CONSUMER_KEY, twitter_credentials.CONSUMER_SECRET)
+        auth.set_access_token(twitter_credentials.ACCESS_TOKEN, twitter_credentials.ACCESS_TOKON_SECRET)
+
+        stream = Stream(auth, listener)
+
+        stream.filter(track=['lebron james', 'kevin durant', 'stephen curry', 'james harden'])
+
         return render(request, self.template_name, {})
 
