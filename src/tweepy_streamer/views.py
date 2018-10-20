f37c7fa017b5c6eaf1be5208924504a1c41d1114
Robert Klim <kontakt.robert.klim@gmail.com>
2018-10-20 14:45:58 +0200

    Some refactoring and writing tweets to json file

--- src/tweepy_streamer/views.py
+++ src/tweepy_streamer/views.py
@@ -10,10 +10,30 @@ from tweepy.streaming import StreamListener
 
 from . import twitter_credentials
 
+class TwitterStreamer():
+
+    def stream_tweets(self, fetched_tweets_filename, hash_tag_list):
+        listener = StdOutListener(fetched_tweets_filename)
+        auth = OAuthHandler(twitter_credentials.CONSUMER_KEY, twitter_credentials.CONSUMER_SECRET)
+        auth.set_access_token(twitter_credentials.ACCESS_TOKEN, twitter_credentials.ACCESS_TOKON_SECRET)
+
+        stream = Stream(auth, listener)
+
+        stream.filter(track=hash_tag_list)
+
 class StdOutListener(StreamListener):
 
+    def __init__(self, fetched_tweets_filename):
+        self.fetched_tweets_filename = fetched_tweets_filename
+
     def on_data(self, data):
-        print(data)
+        try:
+            print('writing data...')
+            with open(self.fetched_tweets_filename, 'a') as tf:
+                tf.write(data)
+            return True
+        except BaseException as e:
+            print(f'Data error:{e}')
         return True
 
     def on_error(self, status):
@@ -23,15 +43,11 @@ class StreamerView(View):
     template_name = 'tweepy_streamer/streamer.html'
 
     def get(self, request, *args, **kwargs):
-        
-        listener = StdOutListener()
-        
-        auth = OAuthHandler(twitter_credentials.CONSUMER_KEY, twitter_credentials.CONSUMER_SECRET)
-        auth.set_access_token(twitter_credentials.ACCESS_TOKEN, twitter_credentials.ACCESS_TOKON_SECRET)
+        hash_tag_list = ['lebrone james', 'kevin durant']
+        fetched_tweets_filename = 'tweets.json'
 
-        stream = Stream(auth, listener)
-
-        stream.filter(track=['lebron james', 'kevin durant', 'stephen curry', 'james harden'])
+        twitter_streamer = TwitterStreamer()
+        twitter_streamer.stream_tweets(fetched_tweets_filename, hash_tag_list)
 
         return render(request, self.template_name, {})
 
