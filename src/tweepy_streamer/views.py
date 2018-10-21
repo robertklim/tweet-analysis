9d30096bbb2859910f54ae7d2d71b0deb1f03e8a
Robert Klim <kontakt.robert.klim@gmail.com>
2018-10-21 18:27:51 +0200

    Add get_friend_list and get_home_timeline_tweets methods to TwitterClient class

--- src/tweepy_streamer/views.py
+++ src/tweepy_streamer/views.py
@@ -24,6 +24,18 @@ class TwitterClient():
             tweets.append(tweet)
         return tweets
 
+    def get_friend_list(self, num_friends):
+        friend_list=[]
+        for friend in Cursor(self.twitter_client.friends, id=self.twitter_user).items(num_friends):
+            friend_list.append(friend)
+        return friend_list
+
+    def get_home_timeline_tweets(self, num_tweets):
+        home_timeline_tweets = []
+        for tweet in Cursor(self.twitter_client.home_timeline, id=self.twitter_user).items(num_tweets):
+            home_timeline_tweets.append(tweet)
+        return home_timeline_tweets
+
 class TwitterAuthenticator():
 
     def authenticate_twitter_app(self):
@@ -70,8 +82,13 @@ class StreamerView(View):
         hash_tag_list = ['lebrone james', 'kevin durant', 'james harden', 'stephen curry']
         fetched_tweets_filename = 'tweets.json'
 
-        twitter_client = TwitterClient('joerogan')
+        twitter_client = TwitterClient()
+        print('-------------- USER TIMELINE')
         print(twitter_client.get_user_timeline_tweets(1))
+        print('-------------- USER FRIEND LIST')
+        print(twitter_client.get_friend_list(1))
+        print('-------------- USER HOME TIMELINE')
+        print(twitter_client.get_home_timeline_tweets(1))
 
         # twitter_streamer = TwitterStreamer()
         # twitter_streamer.stream_tweets(fetched_tweets_filename, hash_tag_list)
