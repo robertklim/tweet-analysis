3e11cc101c2e2a587510043bb160f533d9e787c3
Robert Klim <kontakt.robert.klim@gmail.com>
2018-10-22 11:37:35 +0200

    Install libraries for data analyzis and create TweetAnalyzer class that pulls chosen data from tweets into DataFrame object

--- src/tweepy_streamer/views.py
+++ src/tweepy_streamer/views.py
@@ -11,12 +11,17 @@ from tweepy import Stream
 from tweepy.streaming import StreamListener
 
 from . import twitter_credentials
+import numpy as np
+import pandas as pd
 
 class TwitterClient():
     def __init__(self, twitter_user=None):
         self.auth = TwitterAuthenticator().authenticate_twitter_app()
         self.twitter_client = API(self.auth)
         self.twitter_user = twitter_user
+
+    def get_twitter_client_api(self):
+        return self.twitter_client
 
     def get_user_timeline_tweets(self, num_tweets):
         tweets = []
@@ -75,20 +80,49 @@ class TwitterListener(StreamListener):
             return False
         print(status)
 
+class TweetAnalyzer():
+    
+    def tweets_to_data_frame(self, tweets):
+        df = pd.DataFrame(data=[tweet.text for tweet in tweets], columns=['tweets'])
+        
+        df['id'] = np.array([tweet.id for tweet in tweets])
+        df['len'] = np.array([len(tweet.text) for tweet in tweets])
+        df['date'] = np.array([tweet.created_at for tweet in tweets])
+        df['source'] = np.array([tweet.source for tweet in tweets])
+        df['likes'] = np.array([tweet.favorite_count for tweet in tweets])
+        df['retweets'] = np.array([tweet.retweet_count for tweet in tweets])
+
+        return df
+
 class StreamerView(View):
     template_name = 'tweepy_streamer/streamer.html'
 
     def get(self, request, *args, **kwargs):
-        hash_tag_list = ['lebrone james', 'kevin durant', 'james harden', 'stephen curry']
-        fetched_tweets_filename = 'tweets.json'
 
         twitter_client = TwitterClient()
-        print('-------------- USER TIMELINE')
-        print(twitter_client.get_user_timeline_tweets(1))
-        print('-------------- USER FRIEND LIST')
-        print(twitter_client.get_friend_list(1))
-        print('-------------- USER HOME TIMELINE')
-        print(twitter_client.get_home_timeline_tweets(1))
+        tweet_analyzer = TweetAnalyzer()
+
+        api = twitter_client.get_twitter_client_api()
+
+        tweets = api.user_timeline(screen_name='joerogan', count=20)
+
+        # print(dir(tweets[0]))
+        # print(tweets[0].id)
+        # print(tweets[0].retweet_count)
+
+        df = tweet_analyzer.tweets_to_data_frame(tweets)
+        print(df.head(10))
+
+        # hash_tag_list = ['lebrone james', 'kevin durant', 'james harden', 'stephen curry']
+        # fetched_tweets_filename = 'tweets.json'
+
+        # twitter_client = TwitterClient()
+        # print('-------------- USER TIMELINE')
+        # print(twitter_client.get_user_timeline_tweets(1))
+        # print('-------------- USER FRIEND LIST')
+        # print(twitter_client.get_friend_list(1))
+        # print('-------------- USER HOME TIMELINE')
+        # print(twitter_client.get_home_timeline_tweets(1))
 
         # twitter_streamer = TwitterStreamer()
         # twitter_streamer.stream_tweets(fetched_tweets_filename, hash_tag_list)
